f0bfcdf238888c81f3c65243980cbe5af9ca122a
Boluwatife Ajayi <bolu4good@gmail.com>
2020-06-16 06:27:44 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var songs = ["freeman.mp3", "freeman.mp3", "freeman.mp3", "freeman.mp3"];
+var poster = ["poster.jpg"];
+
+var songTitle = document.getElementById("songTitle");
+var songArtist = document.getElementById("songArtist");
+var fillBar = document.getElementById("fill");
+
+var song = new Audio();
+var currentSong = 0;
+
+window.onload = playSong;
+
+function playSong(){
+    song.src = songs[currentSong];
+    //songTitle.textContent = songs[currentSong];
+    song.play();
+}
+
+function playOrPauseSong(){
+    if(song.paused){
+        song.play();
+        $(".fa-pause i").attr("src", ".fa-play");
+    }
+    else{
+        song.pause();
+        $(".fa-play i").attr("src", ".fa-pause");
+    }
+}
+
+song.addEventListener('timeupdate', function(){
+    var position = song.currentTime / song.duration;
+    fillBar.style.width = position * 100 + '%';
+});
+
+function next() {
+    currentSong++;
+    if(currentSong > 2){
+        currentSong = 0;
+    }
+    playSong();
+    $(".fa-play i").attr("src", ".fa-pause")
+}
+function prev() {
+    currentSong--;
+    if (currentSong < 0) {
+        currentSong = 2;
+    }
+    playSong();
+    $(".fa-play i").attr("src", ".fa-pause")
+}
